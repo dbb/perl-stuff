8069c32726607e166584c17dfcdf3b0f5f0361ac
Daniel Bolton <danielbarrettbolton@gmail.com>
2011-02-20 22:19:06 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# dbbolton's perl-stuff
+
+This is just a collection of Perl cheat-sheets and other reference materials
+that I have found to be useful.
+
+## LICENSE
+
+Copyright © 2011 Daniel Bolton <dan@dbbo.us>.
+
+All contents of this repository are released under the GNU General Public
+License, version 3.
+
+http://www.gnu.org/licenses/gpl.html
+
---
+++ basic-re-cheat.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/perl 
+use strict;
+use warnings;
+use 5.010;
+
+# Metacharacters
+my $metacharacters = ' {} [] () ^ $ . | * + ? \ ' ;
+
+
+# Character classes
+my $char_class = '[]';
+my $range_char = '-';
+say "A Latin letter" if /[A-Za-z]/;
+
+
+# Grouping
+say 'a is $1' if /(a)/;
+
+
+# Anchors
+say 'Starts with "a"' if /^a/;
+say 'Ends with "a"' if /a$/;
+
+
+# Alternation / "or"
+say '"a" or "b"' if /a|b/;
+
+
+# Dot
+my $anything_but_newline = '.';
+
+
+# Counts
+say '"a" zero or more times'  if /a*/;
+say '"a" one ore more times'  if /a+/;
+say 'optional "a"'    if /a?/;
+
+say '"a" exactly 2 times' if /a{2}/;
+say '"a" 3 to 4 times'    if /a{3,4}/;
+say '"a" 5 or more times' if /a{5,}/;
+
+
+# abbreviations
+say 'BOUNDARY'        if /\b/;
+say 'not BOUNDARY'    if /\B/;
+
+say 'DIGIT'           if /\d/;
+say 'not DIGIT'       if /\D/;
+
+say 'WHITESPACE'      if /\s/;
+say 'not WHITESPACE'  if /\S/;
+
+say 'WORDCHAR'        if /\w/;
+say 'not WORDCHAR'    if /\W/;
+
+# a note on '\w' and '\d': they extend beyond [A-Za-z0-9_] and [0-9]
+# respectively (non-Roman letters and non-Arabic digits are included)
+
+# Backreferences
+say '"aa"'    if /(a)\1/;
+say '"abba"'  if /(a)(b)\2\1/;
+say '"abba"'  if /(a)(b)\g{-1}\g{-2}/;
+
+my $named_backref = '(?<nom>abba)';
+say "$+{nom}"   if /$named_backref/;
